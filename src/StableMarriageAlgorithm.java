065be711afd481d691f1fa7c686b563db1953f57
Harout <haroutter@gmail.com>
2015-09-26 18:23:52 -0700

    Read each line into new string array, skip empty lines

--- src/StableMarriageAlgorithm.java
+++ src/StableMarriageAlgorithm.java
@@ -10,6 +10,7 @@ public class StableMarriageAlgorithm {
     private String inputFileName;
     private int n;
 
+
     public StableMarriageAlgorithm(String inputFileName) throws Exception{
         this.inputFileName = inputFileName;
         readInputData();
@@ -25,18 +26,59 @@ public class StableMarriageAlgorithm {
         java.io.File inputFile = new java.io.File(inputFileName);
         Scanner input = new Scanner(inputFile);
         n = input.nextInt();
+        int m = (n+1)*2;
 
-        BufferedReader in = new BufferedReader(new FileReader(inputFileName));
-        String str;
+        //Create an array of m strings
+        String arrayOfStrings[] = new String[m];
 
-        List<String> list = new ArrayList<String>();
-        while((str = in.readLine()) != null){
-            list.add(str);
+
+        try {
+            // Create a bufferreader object to read our file with.
+            BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
+
+            String line = ""; // Line will hold our line read from the file
+
+            int counter = 0; //track num lines read
+
+            /*
+            Read in a line from the file and store it in "line". Do this while we don't hit null or
+            while the counter is less than m. Skip empty lines
+            The counter prevents us from reading in too many lines.
+            */
+            while ((line = reader.readLine()) != null) {
+                if (line.trim().length() == 0) {
+                    continue;
+                }
+                else {
+
+                    while (((line = reader.readLine()) != null) && (counter < m)) {
+                        arrayOfStrings[counter] = line;
+                        counter++;
+                    }
+
+                }
+            }
+
+
+            // print array
+            for (String readline : arrayOfStrings) {
+                System.out.println(readline);
+            }
+
+            reader.close();
+
+            System.out.println();
+            System.out.println(arrayOfStrings[1]);
         }
 
-        String[] stringArr = list.toArray(new String[0]);
 
+        catch (Exception ex) { System.out.println("Exception: " + ex.getMessage()); }
     }
+
+
+
+
+
 
 
 }
