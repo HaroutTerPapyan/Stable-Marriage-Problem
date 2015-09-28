eabacbebe6ea6d6db65a23a463de30db3440d687
Harout <haroutter@gmail.com>
2015-09-27 21:42:24 -0700

    Added testCase4

--- src/SMATest.java
+++ src/SMATest.java
@@ -9,7 +9,11 @@ public class SMATest {
         //StableMarriageAlgorithm sma1 = new StableMarriageAlgorithm("testCase1.txt");
         //StableMarriageAlgorithm sma2 = new StableMarriageAlgorithm("testCase2.txt");
         StableMarriageAlgorithm sma3 = new StableMarriageAlgorithm("testCase3.txt");
+        //StableMarriageAlgorithm sma4 = new StableMarriageAlgorithm("testCase4.txt");
+
 
     }
+
+
 
 }
