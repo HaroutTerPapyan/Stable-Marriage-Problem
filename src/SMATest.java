87a56d4ddfe9f93443f342c6a8e1c24076df81e6
Harout <haroutter@gmail.com>
2015-09-28 22:32:04 -0700

    Cleanup, added more test cases

--- src/SMATest.java
+++ src/SMATest.java
@@ -6,10 +6,31 @@ import java.util.Scanner;
 
 public class SMATest {
     public static void main(String[] args) throws Exception {
-        //StableMarriageAlgorithm sma1 = new StableMarriageAlgorithm("testCase1.txt");
-        //StableMarriageAlgorithm sma2 = new StableMarriageAlgorithm("testCase2.txt");
-        StableMarriageAlgorithm sma3 = new StableMarriageAlgorithm("testCase3.txt");
-        //StableMarriageAlgorithm sma4 = new StableMarriageAlgorithm("testCase4.txt");
+
+       System.out.println("n=6");
+        StableMarriageAlgorithm sma1 = new StableMarriageAlgorithm("testCase1.txt"); //n=6
+        System.out.println("n=9");
+        StableMarriageAlgorithm sma2 = new StableMarriageAlgorithm("testCase2.txt"); //n=9
+        System.out.println("n=3");
+        StableMarriageAlgorithm sma3 = new StableMarriageAlgorithm("testCase3.txt"); //n=3
+        System.out.println("n=8");
+        StableMarriageAlgorithm sma4 = new StableMarriageAlgorithm("testCase4.txt"); //n=8
+        System.out.println("n=10");
+        StableMarriageAlgorithm sma10 = new StableMarriageAlgorithm("testCase10.txt"); //n=10
+        System.out.println("n=11");
+        StableMarriageAlgorithm sma11 = new StableMarriageAlgorithm("testCase11.txt"); //n=11
+        System.out.println("n=12");
+        StableMarriageAlgorithm sma12 = new StableMarriageAlgorithm("testCase12.txt"); //n=12
+        System.out.println("n=50");
+        StableMarriageAlgorithm sma50 = new StableMarriageAlgorithm("testCase50.txt"); //n=50
+        System.out.println("n=100");
+        StableMarriageAlgorithm sma100 = new StableMarriageAlgorithm("testCase100.txt"); //n=100
+        System.out.println("n=200");
+        StableMarriageAlgorithm sma200 = new StableMarriageAlgorithm("testCase200.txt"); //n=200
+        System.out.println("n=300");
+        StableMarriageAlgorithm sma300 = new StableMarriageAlgorithm("testCase300.txt"); //n=300
+        System.out.println("n=400");
+        StableMarriageAlgorithm sma400 = new StableMarriageAlgorithm("testCase400.txt"); //n=400
 
 
     }
